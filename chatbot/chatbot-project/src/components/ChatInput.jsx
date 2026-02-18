b97cf49d06afec19160a1c5dd365bdc47cda5639
meghana123-k <kothakondameghana3@gmail.com>
2026-02-18 23:39:23 +0530

    Updates: 2026-02-18T18:09:21.141Z

--- chatbot/chatbot-project/src/components/ChatInput.jsx
+++ chatbot/chatbot-project/src/components/ChatInput.jsx
@@ -1,10 +1,8 @@
 
 import { useState } from "react";
 import Chatbot from "../lib/chatbot";
-import { ChatMessage } from "./ChatMessage";
-import { ChatMessages } from "./ChatMessages";
 
-export function ChatInput({ chatMessages, setChatMessages }) {
+export default function ChatInput({ chatMessages, setChatMessages }) {
   const [inputText, setInputText] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   function handleChange(event) {
