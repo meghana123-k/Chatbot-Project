5373702b7efcb31a397d46c675a35d51d516818e
meghana123-k <kothakondameghana3@gmail.com>
2026-02-18 23:35:36 +0530

    Updates: 2026-02-18T18:05:34.441Z

---
+++ chatbot/chatbot-project/src/components/ChatMessages.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useRef } from "react";
+
+function ChatMessages({ chatMessages }) {
+  const chatMessagesRef = useRef(null);
+  useEffect(() => {
+    const containerEle = chatMessagesRef.current;
+    if (containerEle) {
+      containerEle.scrollTop = containerEle.scrollHeight;
+    }
+  }, [chatMessages]);
+  return (
+    <div className="chat-messages-container" ref={chatMessagesRef}>
+      {chatMessages.length === 0 ? (
+        <div className="empty-chat">
+          Welcome to the chatbot project! Send a message using the textbox
+          below.
+        </div>
+      ) : (
+        chatMessages.map((chatMessage) => {
+          return (
+            <ChatMessage
+              message={chatMessage.message}
+              sender={chatMessage.sender}
+              key={chatMessage.id}
+            />
+          );
+        })
+      )}
+    </div>
+  );
+}
+export default ChatMessages;
