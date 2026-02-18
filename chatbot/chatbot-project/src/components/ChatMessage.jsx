a5ea0d1e84d3ca1a8482dc6bbab0427497572e8d
meghana123-k <kothakondameghana3@gmail.com>
2026-02-19 00:36:47 +0530

    Added current time feature to messages

--- chatbot/chatbot-project/src/components/ChatMessage.jsx
+++ chatbot/chatbot-project/src/components/ChatMessage.jsx
@@ -1,7 +1,10 @@
 import RobotProfileImage from "../assets/robot.webp";
 import UserProfileImage from "../assets/user.avif";
 import './ChatMessage.css';
+import dayjs from "dayjs";
+
 export default function ChatMessage({ message, sender }) {
+  const time = dayjs().valueOf();
   return (
     <div
       className={sender === "user" ? "chat-message-user" : "chat-message-robot"}
@@ -9,7 +12,11 @@ export default function ChatMessage({ message, sender }) {
       {sender === "robot" && (
         <img src={RobotProfileImage} className="chat-message-profile" />
       )}
-      <div className="chat-message-text">{message}</div>
+      <div className="chat-message-text">{message}
+        <div className="chat-messages-time">
+          {dayjs(time).format('h:mma')}
+        </div>
+      </div>
       {sender === "user" && (
         <img src={UserProfileImage} className="chat-message-profile" />
       )}
